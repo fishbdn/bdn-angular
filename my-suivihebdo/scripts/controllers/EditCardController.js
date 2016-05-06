4de454b6b8aca05dc9dd090aec0ad04bd7d3d61a
fishbdn <florian.rosier@gmail.com>
2016-05-07 01:50:55 +0200

    Project Tasks for a card, and ihm style improvments

--- my-suivihebdo/scripts/controllers/EditCardController.js
+++ my-suivihebdo/scripts/controllers/EditCardController.js
@@ -6,24 +6,43 @@
 
 angular.module('demoApp').controller('EditCardController', ['$scope', '$uibModalInstance', 'column', 'card', function ($scope, $uibModalInstance, column, card) {
 
+  // var vm = this;
+  // var currentCard = 'test';
+  // var currentColumn;
+
+  initScope($scope);
+
+  // console.log("modal $scope", $scope);
+
+
   function initScope(scope) {
-    scope.columnName = column.name;
-    scope.column = column;
-    scope.card = card;
+    console.log("modal $scope", $scope);
+    
+    scope.currentColumn = column;
+    scope.currentCard = card;
+    // currentCard = card;
+    // currentColumn = column;
   }
 
   $scope.editCard = function () {
     if (!this.editCardForm.$valid) {
       return false;
     }
-    $uibModalInstance.close({title: this.title, column: column, card:card});
+    // $uibModalInstance.close({title: this.title, column: column, card:card});
+    
+    // On ne fait rien de particulier, on a tapé direct dans le scope
+    console.log("edit $scope.weekBoard", $scope.weekBoard);
+
+    console.log("edit $scope.currentCard", $scope.currentCard);
+
+    $uibModalInstance.close();
   };
 
   $scope.close = function () {
     $uibModalInstance.close();
   };
 
-  initScope($scope);
+
 
 }]);
 
