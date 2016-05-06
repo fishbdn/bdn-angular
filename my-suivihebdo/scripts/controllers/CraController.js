28bae91bd9a4de8cf6104b6ffc4dfd7084a26012
fishbdn <florian.rosier@gmail.com>
2016-05-06 17:32:30 +0200

    Link Kinvey to Collect Model data from CraController.

--- my-suivihebdo/scripts/controllers/CraController.js
+++ my-suivihebdo/scripts/controllers/CraController.js
@@ -3,13 +3,50 @@
 
 'use strict';
 
-angular.module('demoApp').controller('CraController', ['$scope', 'BoardService', 'BoardDataFactory', function ($scope, BoardService, BoardDataFactory) {
+angular.module('demoApp').controller('CraController', ['$scope', '$kinvey', 'BoardService', 'BoardDataFactory', function ($scope, $kinvey, BoardService, BoardDataFactory) {
 
-  // Load data from BoardDataFactory (static stub)
-  $scope.weekBoard = BoardService.weekBoard(BoardDataFactory.cra);
-  console.log($scope.weekBoard);
 
-  // ng-sortable board options
+  // -------- Load Data -------------- //
+  
+  // ---> From BoardDataFactory (Static)
+  // $scope.weekBoard = BoardService.weekBoard(BoardDataFactory.cra);
+  // $scope.weekBoard = BoardDataFactory.cra;   // Non mapping in model (direct load)
+
+  // ---> Loading from file 
+  /*console.log('Use data stub  : ');
+  $http.get('boardData.json').success(function(data) {
+    var weekBoard = { cra : data };
+    console.log('weekBoard',  weekBoard);
+
+    $scope.weekBoard = weekBoard.cra;
+    console.log('$scope.weekBoard',  $scope.weekBoard);
+  });*/
+
+
+
+  // ---> From External DataStore (Kinvey) 
+  // If user authent asked !?
+  /*
+  var promiseUser = $kinvey.User.login('cracra', 'cracra');
+  promiseUser.then(function(user) {
+    console.log('cra_user signed up !');
+  }, function(err) {
+    console.log('!!! cra_user sign-up failed !');
+  });
+  */
+  var promise = $kinvey.DataStore.get('weekBoard', '572b90ca600943704c60750e');
+  promise.then(function(model) {
+      // console.log("Kinvey GET : ", model);
+      $scope.weekBoard = model;
+  }, function(err) {
+      console.log('Erreur Kinvey : Impossible de recuperer le dataStore !' , err);
+  });
+
+  // console.log($scope.weekBoard);
+
+  
+  // ---- ng-Sortable board config ------- //
+  
   $scope.weekSortOptions = {
     //restrict move across columns. move only within column.
     /*accept: function (sourceItemHandleScope, destSortableScope) {
@@ -23,6 +60,9 @@ angular.module('demoApp').controller('CraController', ['$scope', 'BoardService',
     containment: '#board'
   };
 
+
+  // ************** Services **********************/
+
   $scope.removeCard = function (column, card) {
     BoardService.removeCard($scope.weekBoard, column, card);
   };
@@ -35,6 +75,10 @@ angular.module('demoApp').controller('CraController', ['$scope', 'BoardService',
     console.log("Edit card : "  + column.name)
     BoardService.editCard($scope.weekBoard, column, card);
   }
+
+  // **********************************************/
+
+
 
 }]);
 
